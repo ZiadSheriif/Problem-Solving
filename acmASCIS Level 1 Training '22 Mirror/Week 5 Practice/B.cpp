b38b66ab729c710a5cf2261ea42f258f87c1fbce
ZiadSheriif <zsherif308@gmail.com>
2022-05-25 09:40:22 +0200

    first commit

---
+++ acmASCIS Level 1 Training '22 Mirror/Week 5 Practice/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<climits>
+#include<map>
+#include<set>
+#include<limits.h>
+#include<iomanip>
+
+using namespace std;
+
+#include<string>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for(int i=0;i<v.size();++i)
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define M_PI 3.14159265358979323846
