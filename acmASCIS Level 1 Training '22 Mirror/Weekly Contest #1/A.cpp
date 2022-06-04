662a32c465005e094cfe06f4353a735384e58627
ZiadSheriif <zsherif308@gmail.com>
2022-06-04 15:43:09 +0200

    draft functions & libraries

--- acmASCIS Level 1 Training '22 Mirror/Weekly Contest #1/A.cpp
+++ acmASCIS Level 1 Training '22 Mirror/Weekly Contest #1/A.cpp
@@ -1,34 +1,4 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <climits>
-#include <map>
-#include <set>
-#include <limits.h>
-#include <iomanip>
-
-using namespace std;
-
-#include <string>
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-
-typedef vector<int> vi;
-typedef vector<long long> vll;
-typedef vector<double> vd;
-typedef vector<vi> vvi;
-typedef vector<string> vs;
-#define rep(i, v) for (int i = 0; i < v.size(); ++i)
-
-#define fast                      \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define M_PI 3.14159265358979323846
-ll a, b, c, d, x, y, r, s;
+#include "draft.h"
 void ClimbingForever()
 {
     long long n, a, b;
@@ -42,9 +12,7 @@ void ClimbingForever()
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    fast;
     ClimbingForever();
     return 0;
 }
