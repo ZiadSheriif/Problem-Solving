c0de2c729e6b64b7a8c732b07893efa4b2ded4ee
ZiadSheriif <zsherif308@gmail.com>
2022-06-13 04:13:42 +0200

    Warm Up!

---
+++ acmASCIS Level 1 Training '22 Mirror/Warm Up Sheet/E.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <climits>
+#include <map>
+#include <set>
+#include <limits.h>
+#include <iomanip>
+
+using namespace std;
+
+#include <string>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+#define rep(i, v) for (int i = 0; i < v.size(); ++i)
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define M_PI 3.14159265358979323846
+ll n, x, y, a, b, c, d, s, r, k, w, m, t;
+double o;
+string S;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+int main()
+{
+    fast;
+    cin >> t;
+    vll vec(t);
+    for (ll &x : vec)
+        cin >> x;
+    ll cnt = 0;
+    for (int i = 1; i < t - 1; i++)
+    {
+        if (vec[i] > vec[i + 1] && vec[i] > vec[i - 1])
+            cnt++;
+        else if (vec[i] < vec[i + 1] && vec[i] < vec[i - 1])
+            cnt++;
+    }
+    cout << cnt;
+}
