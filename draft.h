048d54a5cd5015ba02c9a81e26c6173ae4950b23
ZiadSheriif <zsherif308@gmail.com>
2022-07-14 18:04:55 +0200

    🔐 Give Them a Break

--- draft.h
+++ draft.h
@@ -111,3 +111,23 @@ ll nearestPrimeNumber(ll n)
     }
     return 2;
 }
+
+void increasingSub()
+{
+    cin >> n;
+    vector<int> vec(n);
+    vector<int> dp(n, 1);
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (vec[j] < vec[i] && dp[j] >= dp[i])
+            {
+                dp[i] = dp[j] + 1;
+            }
+        }
+    }
+    cout << *max_element(dp.begin(), dp.end());
+}
