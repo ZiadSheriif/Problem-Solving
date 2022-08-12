78157cc1f660c2c31552273e87b1c7b73076063f
ZiadSheriif <zsherif308@gmail.com>
2022-08-12 21:10:45 +0200

    makeEven!

---
+++ acmASCIS Level 1 Training '22 Mirror/Warm Up Sheet/A.cpp
@@ -0,0 +1,202 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <climits>
+#include <map>
+#include <set>
+#include <limits.h>
+#include <iomanip>
+
+using namespace std;
+
+#include <string>
+
+typedef long long ll;
+typedef unsigned long long usll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<pair<ll, ll>> vpll;
+typedef vector<pair<int, int>> vpi;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vll> vvl;
+
+typedef vector<string> vs;
+#define rep(i, v) for (int i = 0; i < v.size(); ++i)
+#define fast ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define M_PI 3.14159265358979323846
+ll n, t, c;
+string S;
+
+ll gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+
+usll factorial(usll n) {
+    if (n == 0)
+        return 1;
+    c = n;
+    for (int i = n - 1; i > 1; i--)
+        c *= i;
+    return c;
+}
+
+bool sortbyCond(const pair<ll, ll> &a, const pair<ll, ll> &b) {
+    if (a.first != b.first)
+        return (a.first < b.first);
+    else
+        return a.second > b.second;
+}
+
+bool sortStrPair(pair<string, pair<ll, ll>> &a, pair<string, pair<ll, ll>> &b) {
+    if (a.second.first != b.second.first)
+        return (a.second.first < b.second.first);
+    else
+        return a.second.second > b.second.second;
+}
+
+bool sortChar(pair<char, ll> &a, pair<char, ll> &b) {
+    if (a.second != b.second)
+        return (a.second > b.second);
+    else
+        return a.first > b.first;
+}
+
+bool primeTest(ll n) {
+
+    bool flag = true;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            flag = false;
+            break;
+        }
+    }
+    if (n <= 1)
+        flag = false;
+    if (flag == 1)
+        return true;
+    return false;
+}
+
+ll nearestPrimeNumber(ll n) {
+    if (n & 1)
+        n -= 2;
+    else
+        n--;
+    int i, j;
+    for (i = n; i >= 2; i -= 2) {
+        if (i % 2 == 0)
+            continue;
+        for (j = 3; j * j <= i; j += 2) {
+            if (i % j == 0)
+                break;
+        }
+        if (j * j > i)
+            return i;
+    }
+    return 2;
+}
+
+void adjacnetList(vector<vector<pair<int, int>>> adjList) {
+    for (int i = 0; i < adjList.size(); ++i) {
+        cout << "Node " << i + 1 << " -->";
+        for (int j = 0; j < adjList[i].size(); ++j) {
+            cout << "{ " << adjList[i][j].first << " , " << adjList[i][j].second << " }";
+        }
+        cout << '\n';
+    }
+}
+
+vector<bool> isVisited(10001);
+
+void dfs(vector<vector<int>> &graph, int node) {
+    cout << node << '\n';
+    isVisited[node] = true;
+    for (auto child: graph[node]) {
+        if (!isVisited[child])
+            dfs(graph, child);
+    }
+}
+
+void bfs(vector<vector<int>> graph, int node) {
+    queue<int> nextToVisit;
+    nextToVisit.push(node);
+    while (!nextToVisit.empty()) {
+        int current = nextToVisit.front();
+        isVisited[current] = true;
+        nextToVisit.pop();
+        cout << current << '\n';
+        for (auto child: graph[current]) {
+            if (!isVisited[child]) {
+                isVisited[child] = true;
+                nextToVisit.push(child);
+            }
+        }
+    }
+}
+
+void solve() {
+
+    //345-->2
+    // 357--->-1
+    // 453-->1
+    // 346-->0
+    ll a, b;
+    cin >> n;
+    ll neg = 0, pos = 0;
+    bool test = false;
+    ll res = n % 10;
+    if (!(res & 1))
+        cout << 0;
+    else {
+        while (n >= 10) {
+            ll temp = n % 10;
+            if (!(temp & 1) && !test)
+                test = true;
+            n /= 10;
+        }
+        if (!(n & 1))
+            cout << 1;
+        else {
+            if (test)
+                cout << 2;
+            else
+                cout << -1;
+        }
+    }
+    cout<<'\n';
+
+
+}
+
+
+int main() {
+    fast;
+    t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+//    int n, m; //n==> # of  nodes , m= # of edges
+//    cin >> n >> m;
+//    vector<vector<int>> adj(n + 1);
+//    for (int i = 0; i < m; ++i) {
+//        int u, v, w;
+//        cin >> u >> v;
+////        adjList[u].push_back({v, w});
+//        adj[u].push_back(v);
+//        adj[v].push_back(u);
+//    }
+//    bfs(adj, 1);
+}
+
+
